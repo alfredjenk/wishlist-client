a26f54844cb28de0996796a7883e263806c3c63b
alfredjenk <a.j.jenkins@wustl.edu>
2024-12-02 13:25:03 -0600

    pull data from firesStore

--- wishlist-client/src/App.jsx
+++ wishlist-client/src/App.jsx
@@ -1,8 +1,30 @@
-import './App.css'
+
+import React, { useEffect, useState } from 'react';
+import './App.css';
 import { Auth } from './components/auth.jsx';
 import { db }from './config/firebase';
+import { getDoc, collection, getDocs } from 'firebase/firestore';
 
 function App() {
+
+  const [itemList, setItemList] = useState([]);
+
+  const itemCollectionRef = collection(db, "item"); // the reference to database
+  
+
+  useEffect( () => {
+    const getItemList = async () => {
+      //read data and set item list
+      try {
+      const data = await getDocs(itemCollectionRef);
+      console.log(data);
+      } catch (err) {
+        console.error(err);
+      }
+    }
+    getItemList();
+  }, []);
+
 
   return (
 
